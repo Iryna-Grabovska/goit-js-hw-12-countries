b495c1d55e750c555314349257f2cb3a54b51914
Iryna-Grabovska <81389070+Iryna-Grabovska@users.noreply.github.com>
2021-09-14 21:28:38 +0000

    Deploying to gh-pages from @ Iryna-Grabovska/goit-js-hw-12-countries@8b8b6a2ef896a52981f456d9af542efde3d9786b 🚀

--- src.f680fa72.js
+++ src.f680fa72.js
@@ -1,7 +1,7 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Tnu0":[function(require,module,exports) {
 
 },{}],"irxn":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;class e{constructor(){this.searchQuery="",this.page=1}fetchCountries(){console.log(this);return fetch("https://restcountries.eu/rest/v2/name/${this.searchQuery}?fields=name;capital;population;languages;flag").then(e=>console.log(e).json()).then(e=>(this.incrementPage(),e.countries))}incrementPage(){this.page=1}resetPage(){this.page=1}get query(){return this.searchQuery}set query(e){this.searchQuery=e}}exports.default=e;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;class e{constructor(){this.searchQuery="",this.page=1}fetchCountries(){console.log(this);return fetch("https://restcountries.eu/rest/v2/name/${this.searchQuery}?fields=name;capital;population;languages;flag").then(e=>e.json()).then(e=>(this.incrementPage(),e.countries))}incrementPage(){this.page=1}resetPage(){this.page=1}get query(){return this.searchQuery}set query(e){this.searchQuery=e}}exports.default=e;
 },{}],"pAws":[function(require,module,exports) {
 var define;
 var global = arguments[3];
@@ -13,4 +13,4 @@ var e,t=arguments[3];!function(t,r){"object"==typeof exports&&"object"==typeof m
 },{"../templates/countries.hbs":"DbMB","./fetchCountries":"irxn"}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.css"),require("./js/fetchCountries"),require("./js/coutries");
 },{"./styles.css":"Tnu0","./js/fetchCountries":"irxn","./js/coutries":"LhTj"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-countries/src.6340c244.js.map
+//# sourceMappingURL=/goit-js-hw-11-countries/src.f680fa72.js.map
