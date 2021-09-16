d2cf9920ac6e42c5e616c467c8a1cd2000cd81ef
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-09-16 23:21:48 +0200

    added max countries

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,24 +1,26 @@
 
 import countriesTpl from "../templates/countries.hbs";
-// import NewsApiService from "./fetchCountries";
 import fetchCountries from './fetchCountries'
 import debounce from "lodash.debounce";
-import { error }  from '@pnotify/core';
-import getRefs from './refs'
-
+import getRefs from './refs';
+import { alert, error, defaultModules } from "@pnotify/core/dist/PNotify.js";
+import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile.js";
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaultModules.set(PNotifyMobile, {});
 const refs = getRefs();
 refs.searchForm.addEventListener('input', debounce(onSearch, 500));
  
 function onSearch(e) {
-  const searchQuery = e.target.value;
-  // const form = e.currentTarget;
-  // const searchQuery = form.input.query.value;
-  // refs.articlesContainer.innerHTML = '';
-  fetchCountries(searchQuery).then(appendCountriesMarkup).catch(searchError).finally(()=>searchQuery.reset());
+  e.preventDefault();
+  refs.articlesContainer.innerHTML = '';
+   fetchCountries(e.target.value).then(appendCountriesMarkup).catch(searchError).finally(()=> (e.target.value = ''));
 }
 
-function searchError( error ) {
-   error({ text: 'Too many matches found.Please enter a more specific query!' });
+function searchError(err) {
+  if (err.message === '404') {
+    error({ text: 'Too many matches found.Please enter a more specific query!' });
+  }
 
 }
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,37 +1,18 @@
-// export default class NewsApiService{
-  
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-//   fetchCountries() {
-//     const url = 'https://restcountries.eu/rest/v2/name/${this.searchQuery}/';
-//   console.log(this)
-//     return fetch(url)
-//       .then(response =>
-//                response.json())
-//      .then(data => {
-//        this.incrementPage()
-//               return data.countries;
 
-//         });
-//   }
-//   incrementPage() {
-//     this.page = 1;
-//   }
-//   resetPage() {
-//     this.page = 1;
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
 export default fetchCountries
 const BASE_URL = 'https://restcountries.eu/rest/v2/name'
 
 function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json())
+  return fetch(`${BASE_URL}/${searchQuery}`).then(response => {
+    if (response.status === 200) {
+    
+      return response.json();
+    } else {
+      throw Error(response.status);
+    }
+  }).then(data => {
+    data.length = 10
+    return data;
+  })
+
 }
